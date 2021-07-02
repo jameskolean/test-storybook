6109b4451d0eec9154b5195eed910282ce638bdd
James Kolean <jameskolean@yahoo.com>
2021-07-02 09:12:27 -0400

    added Storysource addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        loaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    // {
+    //   name: '@storybook/addon-docs',
+    //   options: {
+    //     sourceLoaderOptions: {
+    //       injectStoryParameters: false,
+    //     },
+    //   },
+    // },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
 }
